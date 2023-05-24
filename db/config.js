7b4008e485dec6f05d74e3115d5767cd5d35ae4b
Amit Kumar <gautamak563@gmail.com>
2023-05-24 12:59:13 +0530

    Messages added

--- db/config.js
+++ db/config.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 // mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")
 mongoose.connect("mongodb+srv://gautamak563:0lAg9S4Njy34DdWH@cluster0.i4ywkvj.mongodb.net/e-commerce?retryWrites=true&w=majority")
+    .then(() => console.log("Connected to DB"))
+    .catch(err => console.log("Some error occurred", err));
