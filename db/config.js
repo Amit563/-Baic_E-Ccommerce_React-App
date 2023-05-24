d4dfd3d5c89290b38d1f78cfd39f3bbe3ebe78c0
Amit Kumar <gautamak563@gmail.com>
2023-05-24 12:55:02 +0530

    atlus uri add

--- db/config.js
+++ db/config.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")
-// mongoose.connect("mongodb+srv://gautamak563:0lAg9S4Njy34DdWH@cluster0.i4ywkvj.mongodb.net/e-commerce?retryWrites=true&w=majority")
+// mongoose.connect("mongodb://0.0.0.0:27017/e-commerce")
+mongoose.connect("mongodb+srv://gautamak563:0lAg9S4Njy34DdWH@cluster0.i4ywkvj.mongodb.net/e-commerce?retryWrites=true&w=majority")
